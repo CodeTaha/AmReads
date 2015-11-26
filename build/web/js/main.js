f24b619dba85c4abe486dc26d4bcf2eae826aae7
Codetaha <taha.kachwala21@gmail.com>
2015-11-26 03:38:23 +0200

    Web service created

---
+++ build/web/js/main.js
@@ -0,0 +1,103 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function() {
+      _.templateSettings = {
+        interpolate: /\{\{(.+?)\}\}/g
+      };
+      search();
+    });
+    
+    var search = function() {
+      $.ajax({
+        url: "webresources/amazonClient/itemSearch?keywords="+$("#searchBox").val(),
+        type: "POST", //send it through POST method
+        data:{keywords:$("#searchBox").val()},
+        success: function(response) {
+          console.log("response=",response);
+          createDivs(response.ItemSearchResponse.Items);
+          
+
+        }
+      });  
+    };
+    
+    var createDivs = function(data){
+      var template2=_.template("<div class=\"col-sm-4 col-md-3\">\n\
+        <div class=\"thumbnail\">\n\
+          <img src=\"{{img_url}}\" alt=\"...\">\n\
+          <div class=\"caption\">\n\
+            <h3>{{title}}</h3>\n\
+            <p>Price: {{list_price}}</p>\n\
+            <p><a href=\"#\" class=\"btn btn-success\" role=\"button\">Button</a>\n\
+              <button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#myModal\" onclick=\"fillModal('#fillm_{{ISBN}}')\" id='fillm_{{ISBN}}'>\n\
+                More..\n\
+              </button>\n\
+            </p>\n\
+          </div>\n\
+        </div>\n\
+      </div>");
+      $("#container").empty();
+      data.Item.forEach(function(item){
+        //console.log(item);
+        var ListPrice = typeof item.ItemAttributes.FormattedPrice !== 'undefined' ? item.ItemAttributes.ListPrice.Amount : "N/A";
+        
+        $("#container").append(template2({
+          title: item.ItemAttributes.Title,
+          img_url:item.MediumImage.URL,
+          list_price:ListPrice,
+          ISBN: item.ItemAttributes.ISBN//JSON.stringify(item)
+        }));
+        $("#fillm_"+item.ItemAttributes.ISBN).data(item);
+        //console.log("#fillm_"+item.ItemAttributes.ISBN)
+      });
+    };
+    
+    var fillModal = function(datares){
+      var item = $(datares).data();
+      console.log("enjoy", item);
+      var goodread;
+      $("#myModalLabel").html(item.ItemAttributes.Title);
+      $.ajax({
+        url: "http://localhost:8084/BookRead/webresources/amazonClient/getreview/"+item.ItemAttributes.ISBN,
+        type: "GET",
+        success: function(gread) {
+          goodread=gread;
+          console.log("goodread=",goodread);
+          $("#myModalBody").append(template3({
+          img_url:  item.MediumImage.URL,
+          list_price: ListPrice,
+          details:  item.DetailPageURL,
+          isbn: item.ItemAttributes.ISBN,
+          author: item.ItemAttributes.Author,
+          gr:goodread.reviews_widget
+          //ISBN: item.ItemAttributes.ISBN//JSON.stringify(item)
+        }));
+        }
+      });
+      var ListPrice = typeof item.ItemAttributes.ListPrice !== 'undefined' ? item.ItemAttributes.ListPrice.FormattedPrice : "N/A";
+      var template3= _.template('\
+            <div class="thumbnail">\n\
+              <img src="{{img_url}}" alt="...">\n\
+              <div class="caption">\n\
+                <h3>Thumbnail label</h3>\n\
+                <div class="row">\n\
+                    <div class="col-md-3 col-sm-3">Price: {{list_price}}</div>\n\
+                    <div class="col-md-3 col-sm-3"><b>Author:</b>{{author}}</div>\n\
+                    <div class="col-md-3 col-sm-3"><b>ISBN:</b>{{isbn}}</div>\n\
+                    <div class="col-md-3 col-sm-3"><a target="_blank" href="{{details}}">More info</a></div>\n\
+                    <div class="col-md-12 col-sm-12">{{gr}}</div>\n\
+                </div>\n\
+              </div>\n\
+            </div>\n\
+            <div class="modal-footer">\n\
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n\
+              <button type="button" class="btn btn-primary">Save changes</button>\n\
+            </div>');
+      $("#myModalBody").empty();
+      
+    };
---
+++ src/java/com/bookread/rs/AmazonClientResource.java
@@ -0,0 +1,213 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookread.rs;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.POST;
+import javax.ws.rs.QueryParam;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import javax.ws.rs.PathParam;
+import org.json.JSONObject;
+import org.json.XML;
+
+//import net.sf.json.JSON
+/**
+ * REST Web Service
+ *
+ * @author taha
+ */
+@Path("amazonClient")
+public class AmazonClientResource implements configAWS{
+    
+    /*
+     * Set up the signed requests helper.
+     */
+  Gson gson = new Gson();
+  private SignedRequestsHelper helper;
+  String requestUrl = null;
+  @Context
+  private UriInfo context;
+
+  /**
+   * Creates a new instance of AmazonClientResource
+   */
+  public AmazonClientResource() {
+     try {
+          helper = SignedRequestsHelper.getInstance(ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+            System.out.println("Helper created successfully");
+     } catch (IllegalArgumentException | UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException e) {
+       Logger.getLogger(AmazonClientResource.class.getName()).log(Level.SEVERE, null, e);
+     }
+            
+
+        
+  }
+  /**
+   * Retrieves representation of an instance of com.bookread.rs.AmazonClientResource
+   * @return an instance of java.lang.String
+   */
+  @POST
+  @Produces("application/json")
+  @Path("/itemSearch")
+  public String itemSearch(
+          @DefaultValue("J.K Rowling") @QueryParam("keywords") String keywords,
+          @DefaultValue("price") @QueryParam("SortBy") String SortBy
+          
+  ) {
+    System.out.println("Keyword"+keywords);
+    //TODO return proper representation object
+    try{
+      Map<String, String> params = new HashMap<String, String>();
+
+      params.put("Service", "AWSECommerceService");
+      params.put("Operation", "ItemSearch");
+      params.put("AWSAccessKeyId", AWS_ACCESS_KEY_ID);
+      params.put("AssociateTag", AssociateTag);
+      params.put("SearchIndex", "Books");
+      params.put("Keywords", keywords);
+      params.put("ResponseGroup", "Images,ItemAttributes, OfferSummary");
+      params.put("Sort", SortBy);
+
+      requestUrl = helper.sign(params);
+
+      System.out.println("Signed URL: \"" + requestUrl + "\"");
+      return sendGet(requestUrl);
+    } catch (Exception e){
+      return "Failed";
+    }
+    
+  }
+  /**
+   * Retrieves representation of an instance of com.bookread.rs.AmazonClientResource
+   * @param isbn
+   * @return an instance of java.lang.String
+   */
+  @GET
+  @Produces("application/json")
+  @Path("/getreview/{isbn}")
+  public String getreview(@PathParam("isbn") String isbn) {
+    String url="http://www.goodreads.com/book/isbn?format=json&isbn="+isbn+"&key="+GoodReadKey;
+    try{
+    URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+    if(responseCode!=200){
+      return "{\"error\":\"No Reviews\"}";
+    }
+    StringBuffer response;
+    try (BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()))) {
+      String inputLine;
+      response = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+    }
+    String resp = response.toString();
+    //System.out.println("GR json"+resp);
+    return resp;
+    } catch(Exception e){
+      System.out.println(e);
+    return "{\"error\":\"No Reviews\"}";
+    }
+  }
+  /**
+   * Retrieves representation of an instance of com.bookread.rs.AmazonClientResource
+   * @return an instance of java.lang.String
+   */
+  @GET
+  @Produces("application/json")
+  public String getJson() {
+    //TODO return proper representation object
+    return "";
+  }
+  
+
+  /**
+   * PUT method for updating or creating an instance of AmazonClientResource
+   * @param content representation for the resource
+   */
+  @PUT
+  @Consumes("application/json")
+  public void putJson(String content) {
+  }
+  /* 
+    For sendng remote GET requests to Amazon
+  */
+  public String sendGet(String url) throws Exception {
+
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+    
+    StringBuffer response;
+    try (BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()))) {
+      String inputLine;
+      response = new StringBuffer();
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+    }
+
+		//print result
+    String respXML = response.toString();
+		//System.out.println("XML op"+respXML);
+    try {
+      JSONObject JSONObj = XML.toJSONObject(respXML);
+      
+      System.out.println(JSONObj.length());
+      String json = JSONObj.toString(4);
+      return json;
+    } catch (Exception e){
+      System.out.println("Not that great"+e);
+      return "Failed";
+    }
+	}
+
+}
