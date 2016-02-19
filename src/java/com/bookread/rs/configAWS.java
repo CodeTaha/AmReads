7d1050dd69658c5068a083f9ea3a51b98480ee4b
Codetaha <taha.kachwala21@gmail.com>
2016-02-19 23:39:17 +0200

    Adding configAWS

---
+++ src/java/com/bookread/rs/configAWS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookread.rs;
+
+/**
+ *
+ * @author taha
+ */
+public interface configAWS {
+  // WARNING!!!! NEVER POST THIS FILE ON ANY PUBLIC REPOSITORIES
+  static final String AWS_ACCESS_KEY_ID = "PUT YOUR CREDS";
+  static final String AWS_SECRET_KEY = "PUT YOUR CREDS";
+  static final String ENDPOINT = "webservices.amazon.co.uk";
+  static final String AssociateTag = "PUT YOUR CREDS";
+  static final String GoodReadKey = "PUT YOUR CREDS";
+  
+  // client secret and id for Amreads Transactions
+  static final int AmReads_client_id=8;
+  static final String AmReads_client_secret="pb7a0pgsig9cedotn0r8btpq09";
+  
+}
